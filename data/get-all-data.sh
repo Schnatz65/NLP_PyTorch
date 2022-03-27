1f17ae7ed550bfde55d08040de89b23f5c7e353b
Schnatz65 <95356056+Schnatz65@users.noreply.github.com>
2022-03-27 18:50:31 +0200

    Update get-all-data.sh

--- data/get-all-data.sh
+++ data/get-all-data.sh
@@ -53,4 +53,4 @@ if [ ! -f $HERE/nmt/simplest_eng_fra.csv ]; then
     python $HERE/download.py 1jLx6dZllBQ3LXZkCjZ4VciMQkZUInU10 $HERE/nmt/simplest_eng_fra.csv # 30
 fi 
 
-mkdir -p $HERE/glove.6b
+mkdir -p $HERE/glove.6B
